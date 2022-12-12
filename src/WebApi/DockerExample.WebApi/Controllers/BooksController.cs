afa4c36c3ce58ab51829ea09f708c0e576f81712
hakannyucel <hakanyucelsw@gmail.com>
2022-12-12 22:42:35 +0300

    Add WebApi Project

---
+++ src/WebApi/DockerExample.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using DockerExample.Common.Models.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerExample.WebApi.Controllers
+{
+  [Route("api/v1/[controller]")]
+  [ApiController]
+  public class BooksController : ControllerBase
+  {
+    private readonly IMediator _mediator;
+
+    public BooksController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetBookListAsync()
+    {
+      return Ok(await _mediator.Send(new GetBookListQuery()));
+    }
+  }
+}
--- src/WebApi/DockerExample.WebApi/Program.cs
+++ src/WebApi/DockerExample.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using DockerExample.Application;
+using DockerExample.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
